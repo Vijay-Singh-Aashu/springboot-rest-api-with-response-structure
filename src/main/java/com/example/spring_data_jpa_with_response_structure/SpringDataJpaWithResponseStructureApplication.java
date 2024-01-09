f7bd241676e946e69367ab12b0885681f3708330
Vijay Singh <vijaysinghaashu@gmail.com>
2024-01-09 11:21:38 +0530

    initialize the project

---
+++ src/main/java/com/example/spring_data_jpa_with_response_structure/SpringDataJpaWithResponseStructureApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_with_response_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaWithResponseStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaWithResponseStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_data_jpa_with_response_structure/SpringDataJpaWithResponseStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_with_response_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaWithResponseStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
